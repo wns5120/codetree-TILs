c0e0edc340df1914bea471a6cd9fa7c62f5f0ebb
wns5120 <73707598+wns5120@users.noreply.github.com>
2023-11-10 22:18:11 +0900

    Title: X 달리기, Time: 106ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231110/X 달리기/x-run.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int num = (int) Math.floor(Math.sqrt(x));
+        int ans = 0;
+
+        ans = num * 2 - 1;
+
+        int sum = 0;
+        for (int i = 1; i <= num; i++)
+            sum += i;
+
+        for (int i = num - 1; i >= 1; i--)
+            sum += i;
+
+        ans+=(x-sum)/num;
+        System.out.println(ans);
+    }
+
+}
