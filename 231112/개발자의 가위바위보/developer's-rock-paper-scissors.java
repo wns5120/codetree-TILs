2acf1715ddef9d382e2938cb0ccfc6e7e058b33a
wns5120 <73707598+wns5120@users.noreply.github.com>
2023-11-12 19:03:44 +0900

    Title: 개발자의 가위바위보, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231112/개발자의 가위바위보/developer's-rock-paper-scissors.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int aw = 0;
+        int bw = 0;
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 1) {
+                if (b[i] == 2)
+                    aw++;
+                else if (b[i] == 3)
+                    bw++;
+            } else if (a[i] == 2) {
+                if (b[i] == 3)
+                    aw++;
+                else if (b[i] == 1)
+                    aw++;
+            } else if (a[i] == 3) {
+                if (b[i] == 1)
+                    aw++;
+                else if (b[i] == 2)
+                    bw++;
+            }
+        }
+        System.out.println(Math.max(aw, bw));
+    }
+
+}
