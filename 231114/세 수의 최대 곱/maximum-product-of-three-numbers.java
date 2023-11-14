ed4943e7de5f9cac64afe36dfef58053f448a5f5
wns5120 <73707598+wns5120@users.noreply.github.com>
2023-11-14 22:21:12 +0900

    Title: 세 수의 최대 곱, Time: 817ms, Memory: 36MB, Status: Passed - CodeTree

--- 231114/세 수의 최대 곱/maximum-product-of-three-numbers.java
+++ 231114/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -2,24 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int[] arr = new int[100000];
+    public static int n;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int ans = Integer.MIN_VALUE;
+        n = sc.nextInt();
 
-        for (int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    ans = Math.max(ans, arr[i] * arr[j] * arr[k]);
-                }
-            }
+            arr[i] = sc.nextInt();
         }
-        System.out.println(ans);
+
+        Arrays.sort(arr, 0, n);
+        
+
+        if (arr[0] * arr[1] > arr[n - 2] * arr[n - 3]) {
+            System.out.println(arr[0] * arr[1] * arr[n - 1]);
+        } else
+            System.out.println(arr[n - 1] * arr[n - 2] * arr[n - 3]);
+
     }
 
 }
