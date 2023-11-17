e18aa64bc8b60423ae209b78e0ea9672cb8da91a
wns5120 <73707598+wns5120@users.noreply.github.com>
2023-11-17 16:21:35 +0900

    Title: 최소 와이파이 수, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231117/최소 와이파이 수/minimum-number-of-wifi.java
+++ 231117/최소 와이파이 수/minimum-number-of-wifi.java
@@ -8,7 +8,7 @@ public class Main {
         int n = sc.nextInt();
         int[] arr = new int[n];
         int m = sc.nextInt();
-        int[] wifi = new int[n+100];
+        int[] wifi = new int[n + 100];
 
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
@@ -16,16 +16,16 @@ public class Main {
         for (int i = 0; i < n; i++) {
             if (arr[i] == 1) {
                 wifi[i + m] = 1;
-                i += (m + 1);
+                i += m;
             }
         }
 
         int ans = 0;
 
-        for (int i = 0; i < n+100; i++)
+        for (int i = 0; i < n; i++)
             if (wifi[i] == 1)
                 ans++;
-
+            
         System.out.println(ans);
     }
 
