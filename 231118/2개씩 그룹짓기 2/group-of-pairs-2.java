52be6ecc2c6fe339f314d5ccd0c9830396e056de
wns5120 <73707598+wns5120@users.noreply.github.com>
2023-11-18 23:14:35 +0900

    Title: 2개씩 그룹짓기 2, Time: 1115ms, Memory: 52MB, Status: Passed - CodeTree

--- 231118/2개씩 그룹짓기 2/group-of-pairs-2.java
+++ 231118/2개씩 그룹짓기 2/group-of-pairs-2.java
@@ -1,24 +1,29 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 100000;
+    
+    public static int n;
+    public static int[] arr = new int[2 * MAX_N];
+    public static int ans = INT_MAX;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] arr = new int[2 * n];
-        int ans = Integer.MAX_VALUE;
-
-        for (int i = 0; i < 2 * n; i++)
+        // 입력
+        n = sc.nextInt();
+        for(int i = 1; i <= 2 * n; i++)
             arr[i] = sc.nextInt();
 
-        Arrays.sort(arr);
+        Arrays.sort(arr, 1, 2 * n + 1);
 
-        for (int i = 0; i < n; i++) {
-            ans = Math.min(ans, arr[i + n] - arr[i]);
+        // 정렬한 뒤 1번째 값과 n + 1번째 값, 2번째 값과 n + 2번째 값, ... n번째 값과 2n번째 값의
+        // 차이를 구합니다. 구한 값들의 최솟값을 찾습니다.
+        for(int i = 1; i <= n; i++) {
+            ans = Math.min(ans, arr[n + i] - arr[i]);
         }
 
-        System.out.println(ans);
+        System.out.print(ans);
     }
-
 }
