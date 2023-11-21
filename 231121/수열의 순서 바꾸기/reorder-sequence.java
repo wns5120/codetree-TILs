86fbac90326532f94c6b33d029b0849d59d8408b
wns5120 <73707598+wns5120@users.noreply.github.com>
2023-11-21 18:17:29 +0900

    Title: 수열의 순서 바꾸기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231121/수열의 순서 바꾸기/reorder-sequence.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        for (int i = n - 1; i >= 1; i--) {
+            if (arr[i] < arr[i - 1])
+                ans = i;
+        }
+
+        System.out.println(ans);
+    }
+
+}
