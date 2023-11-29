5d356de590abc4ebb9a44c50fc1a76b31d4a4b2d
wns5120 <73707598+wns5120@users.noreply.github.com>
2023-11-29 10:55:34 +0900

    Title: 구간 잘 나누기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 231129/구간 잘 나누기/divide-sections-well.java
+++ 231129/구간 잘 나누기/divide-sections-well.java
@@ -8,30 +8,31 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] arr = new int[n];
-        int ans = Integer.MAX_VALUE;
-        int max=0;
+        int max = 0;
 
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
 
-        for(int i=0;i<n;i++)
-            max=Math.max(arr[i],max);
+        for (int i = 0; i < n; i++)
+            max = Math.max(arr[i], max);
 
         for (int i = max; i < 100 * 100 + 1; i++) {
             int cnt = 0;
             int sum = arr[0];
             for (int j = 1; j < n; j++) {
-                if (sum + arr[j] < i) {
+                if (sum + arr[j] <= i) {
                     sum += arr[j];
                 } else {
                     cnt++;
                     sum = arr[j];
                 }
             }
-            if (cnt <= m)
-                ans = Math.min(ans, i);
+            if (cnt <= m-1) {
+                System.out.println(i);
+                break;
+            }
         }
-        System.out.println(ans);
+       
     }
 
 }
