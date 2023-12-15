05116e45d3c834651cc9a473508f93badbf0f276
HoJun <73707598+wns5120@users.noreply.github.com>
2023-12-15 21:17:30 +0900

    Title: 퀵 정렬 구현, Time: 1469ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 231215/퀵 정렬 구현/implement-quick-sort.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+    public static int[] arr;
+
+    public static int part(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+            
+        }
+        int tmp = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = tmp;
+            
+        return i + 1;
+    }
+
+    public static void quick_sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pos = part(arr, low, high);
+
+            quick_sort(arr, low, pos - 1);
+            quick_sort(arr, pos + 1, high);
+        }
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        quick_sort(arr, 0, n - 1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+
+    }
+
+}
